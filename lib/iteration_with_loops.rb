1156835bf31146c0b300b9ae35abf84e019ab434
Saima Akhtar <saimaakhtar@gmail.com>
2020-03-30 22:59:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while i < src.length do
   element_i = 0 
   while element_i < src[row_i].length do
     if src[row_i][element_i].even ? 
-      p src[row_i][element_i]
+      puts src[row_i][element_i]
     end
   element_i += 1 
  end
